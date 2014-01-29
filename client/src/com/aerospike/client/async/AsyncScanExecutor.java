f274896bdccb37ab747397715795621f4ed7863e
Brian Nichols <bnichols@aerospike.com>
2014-01-29 10:20:26 -0800

    Throw exception when running scan/query and the cluster is empty.

--- client/src/com/aerospike/client/async/AsyncScanExecutor.java
+++ client/src/com/aerospike/client/async/AsyncScanExecutor.java
@@ -10,6 +10,7 @@
 package com.aerospike.client.async;
 
 import com.aerospike.client.AerospikeException;
+import com.aerospike.client.ResultCode;
 import com.aerospike.client.cluster.Node;
 import com.aerospike.client.listener.RecordSequenceListener;
 import com.aerospike.client.policy.ScanPolicy;
@@ -28,6 +29,10 @@ public final class AsyncScanExecutor extends AsyncMultiExecutor {
 		this.listener = listener;
 
 		Node[] nodes = cluster.getNodes();
+		if (nodes.length == 0) {
+			throw new AerospikeException(ResultCode.SERVER_NOT_AVAILABLE, "Scan failed because cluster is empty.");
+		}
+
 		completedSize = nodes.length;
 
 		for (Node node : nodes) {			
