00790cf5241134eaaa4a5b0b8a5c6b22bb7b5d04
Tobin Lehman <toby@aerospike.com>
2014-08-01 14:09:04 -0700

    LDT Java Client: Show example of larger scale insert for LLIST.

--- examples/src/com/aerospike/examples/LargeList.java
+++ examples/src/com/aerospike/examples/LargeList.java
@@ -99,5 +99,15 @@ public class LargeList extends Example {
 		else {
 			console.error("Data mismatch: Expected %s. Received %s.", expected, stringReceived);
 		}
+		
+		// Add a bunch of numbers to show volume insert (add in reverse order)
+		int itemCount = 2000;
+		console.info("Add a bunch of LLIST Items(%d)", itemCount);
+		com.aerospike.client.large.LargeList llist2 = client.getLargeList(params.policy, key, "NumberBin", null);
+		for (int i = itemCount; i > 0; i-- ){
+			llist2.add(Value.get(i));
+		}
+		int size2 = llist2.size();
+		console.info("Done Writing (%d) Items, size(%d)", itemCount, size2 );
 	}	
 }
