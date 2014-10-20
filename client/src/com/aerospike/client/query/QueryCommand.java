7eefe470069e5b8cb9a1f1cb402085e8338d9183
Brian Nichols <bnichols@aerospike.com>
2014-10-20 10:41:50 -0700

    Always send taskId on queries.

--- client/src/com/aerospike/client/query/QueryCommand.java
+++ client/src/com/aerospike/client/query/QueryCommand.java
@@ -94,10 +94,9 @@ public abstract class QueryCommand extends MultiCommand {
 			fieldCount++;
 		}
 		
-		if (statement.taskId > 0) {
-			dataOffset += 8 + FIELD_HEADER_SIZE;
-			fieldCount++;
-		}
+		// Allocate space for TaskId field.
+		dataOffset += 8 + FIELD_HEADER_SIZE;
+		fieldCount++;
 		
 		if (statement.functionName != null) {
 			dataOffset += FIELD_HEADER_SIZE + 1;  // udf type
@@ -168,12 +167,11 @@ public abstract class QueryCommand extends MultiCommand {
 			dataBuffer[dataOffset++] = priority;
 			dataBuffer[dataOffset++] = (byte)100;			
 		}
-		
-		if (statement.taskId > 0) {
-			writeFieldHeader(8, FieldType.TRAN_ID);
-			Buffer.longToBytes(statement.taskId, dataBuffer, dataOffset);
-			dataOffset += 8;
-		}
+
+		// Write taskId field
+		writeFieldHeader(8, FieldType.TRAN_ID);
+		Buffer.longToBytes(statement.taskId, dataBuffer, dataOffset);
+		dataOffset += 8;
 		
 		if (statement.functionName != null) {
 			writeFieldHeader(1, FieldType.UDF_OP);
