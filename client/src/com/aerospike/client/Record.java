b46820e33323d9eef43bf741ae54d167cb431593
Brian Nichols <bnichols@aerospike.com>
2015-06-11 10:57:05 -0700

    Add Record.getTimeToLive()

--- client/src/com/aerospike/client/Record.java
+++ client/src/com/aerospike/client/Record.java
@@ -123,6 +123,27 @@ public final class Record {
 		// The server always returns booleans as longs, so get long and convert.
 		return (getLong(name) != 0) ? true : false;
 	}
+	
+	/**
+	 * Convert record expiration (seconds from Jan 01 2010 00:00:00 GMT) to
+	 * ttl (seconds from now).
+	 */
+	public int getTimeToLive() {
+		// This is the server's flag indicating the record never expires.
+		if (expiration == 0) {
+			// Convert to client-side convention for "never expires".
+			return -1;
+		}
+		
+		// Subtract milliseconds (1970/1/1 GMT to 2010/1/1 GMT) from current time.
+		// Handle server's unsigned int ttl with java's usage of long for time.
+		int now = (int)((System.currentTimeMillis() - 1262304000000L) / 1000);
+		
+		// Record may not have expired on server, but delay or clock differences may
+		// cause it to look expired on client. Floor at 1, not 0, to avoid old
+		// "never expires" interpretation.
+		return (expiration < 0 || expiration > now) ? expiration - now : 1;
+	}
 
 	/**
 	 * Return String representation of record.
