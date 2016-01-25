d576b664c81974b713d3be9ae682549ad95c78b2
Brian Nichols <bnichols@aerospike.com>
2016-01-25 13:53:21 -0800

    minor javadoc fix.

--- client/src/com/aerospike/client/Bin.java
+++ client/src/com/aerospike/client/Bin.java
@@ -292,6 +292,7 @@ public final class Bin {
 	 * Create bin with a GeoJSON value.
 	 * 
 	 * @param name		bin name, current limit is 14 characters
+	 * @param value		bin value
 	 */
 	public static Bin asGeoJSON(String name, String value) {
 		return new Bin(name, Value.getAsGeoJSON(value));
