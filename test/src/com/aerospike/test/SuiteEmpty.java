e0e67b5c106f63249eb9f55f5ae585c1f5d5879f
Minting Xiao <mxiao@aerospike.com>
2017-01-11 18:54:11 -0800

    Added empty test suite

---
+++ test/src/com/aerospike/test/SuiteEmpty.java
@@ -0,0 +1,12 @@
+package com.aerospike.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+})
+
+public class SuiteEmpty {
+
+}
