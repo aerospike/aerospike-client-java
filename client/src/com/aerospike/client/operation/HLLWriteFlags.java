3b430bee159249db5d75e79420d61a2b939fa058
Piyush Gupta <pygupta@users.noreply.github.com>
2023-02-06 16:34:50 -0800

    Update HLLWriteFlags.java

--- client/src/com/aerospike/client/operation/HLLWriteFlags.java
+++ client/src/com/aerospike/client/operation/HLLWriteFlags.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2021 Aerospike, Inc.
+ * Copyright 2012-2023 Aerospike, Inc.
  *
  * Portions may be licensed to Aerospike, Inc. under one or more contributor
  * license agreements WHICH ARE COMPATIBLE WITH THE APACHE LICENSE, VERSION 2.0.
@@ -17,9 +17,11 @@
 package com.aerospike.client.operation;
 
 /**
- * HyperLogLog operation policy write flags.
- * Use BITWISE OR to combine flags.
- * e.g. HLLWriteFlags.CREATE_ONLY | HLLWriteFlags.NO_FAIL
+ * HyperLogLog operation policy write bit flags. Use BITWISE OR to combine flags. Example:
+ * 
+ * <pre>{@code 
+ * int flags = HLLWriteFlags.CREATE_ONLY | HLLWriteFlags.NO_FAIL;
+ * }</pre>
  */
 public final class HLLWriteFlags {
 	/**
