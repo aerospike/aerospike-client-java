a9c1f7290e08d39a63adde1c84ecb509da77cea2
ashishshinde <ashish@aerospike.com>
2023-03-14 12:47:14 -0700

    Increased incoming message limit and event loop fixes
    - Increased the incoming payload size to 128MB
    - onError and onNext were at times called from non-event loop threads. Scheduled these calls on event loop to ensure thread safety.
    - minor formatting change
    - allowed sync calls from benchmarks
    - minor formatting fixes

--- benchmarks/src/com/aerospike/benchmarks/Main.java
+++ benchmarks/src/com/aerospike/benchmarks/Main.java
@@ -1200,7 +1200,9 @@ public class Main implements Log.Callback {
 			}
 		}
 		else {
-			IAerospikeClient client = new AerospikeClient(clientPolicy, hosts);
+			IAerospikeClient client = useProxyClient?
+				new AerospikeClientProxy(clientPolicy, hosts) :
+				new AerospikeClient(clientPolicy, hosts);
 
 			try {
 				if (initialize) {
@@ -1226,7 +1228,7 @@ public class Main implements Log.Callback {
 		long rem = this.nKeys - (keysPerTask * ntasks);
 		long start = this.startKey;
 
-		for (long i = 0 ; i < ntasks; i++) {
+		for (long i = 0; i < ntasks; i++) {
 			long keyCount = (i < rem)? keysPerTask + 1 : keysPerTask;
 			InsertTaskSync it = new InsertTaskSync(client, args, counters, start, keyCount);
 			es.execute(it);
--- proxy/src/com/aerospike/client/proxy/grpc/GrpcChannelExecutor.java
+++ proxy/src/com/aerospike/client/proxy/grpc/GrpcChannelExecutor.java
@@ -80,10 +80,10 @@ public class GrpcChannelExecutor implements Runnable {
 	 * in TLS verification of the proxy server.
 	 */
 	public static final String OVERRIDE_AUTHORITY = "com.aerospike.client" +
-			".overrideAuthority";
+		".overrideAuthority";
 
 	private static final String AEROSPIKE_CLIENT_USER_AGENT =
-			"AerospikeClientJava/" + AerospikeClientProxy.Version;
+		"AerospikeClientJava/" + AerospikeClientProxy.Version;
 
 	/**
 	 * Call queue initial chunk size.
@@ -96,6 +96,7 @@ public class GrpcChannelExecutor implements Runnable {
 	 * TODO: how to select interval of execution?
 	 */
 	private static final long ITERATION_DELAY_MICROS = 250;
+
 	/**
 	 * Unique executor ids.
 	 */
@@ -124,12 +125,12 @@ public class GrpcChannelExecutor implements Runnable {
 	 * Queued unary calls awaiting execution.
 	 */
 	private final MpscUnboundedArrayQueue<GrpcStreamingCall> pendingCalls =
-			new MpscUnboundedArrayQueue<>(32);
+		new MpscUnboundedArrayQueue<>(32);
 	/**
 	 * Queue of closed streams.
 	 */
 	private final MpscUnboundedArrayQueue<GrpcStream> closedStreams =
-			new MpscUnboundedArrayQueue<>(32);
+		new MpscUnboundedArrayQueue<>(32);
 	/**
 	 * Map of stream id to streams.
 	 */
@@ -178,18 +179,18 @@ public class GrpcChannelExecutor implements Runnable {
 
 		this.grpcClientPolicy = grpcClientPolicy;
 		this.drainLimit =
-				this.grpcClientPolicy.maxConcurrentStreamsPerChannel * grpcClientPolicy.maxConcurrentRequestsPerStream;
+			this.grpcClientPolicy.maxConcurrentStreamsPerChannel * grpcClientPolicy.maxConcurrentRequestsPerStream;
 		this.authTokenManager = authTokenManager;
 		this.id = executorIdIndex.getAndIncrement();
 		ChannelAndEventLoop channelAndEventLoop =
-				createGrpcChannel(channelTypeAndEventLoop.getEventLoop()
-						, channelTypeAndEventLoop.getChannelType(), hosts);
+			createGrpcChannel(channelTypeAndEventLoop.getEventLoop()
+				, channelTypeAndEventLoop.getChannelType(), hosts);
 		this.channel = channelAndEventLoop.managedChannel;
 		this.eventLoop = channelAndEventLoop.eventLoop;
 
 		ScheduledFuture<?> future =
-				channelAndEventLoop.eventLoop.scheduleAtFixedRate(this, 0,
-						ITERATION_DELAY_MICROS, TimeUnit.MICROSECONDS);
+			channelAndEventLoop.eventLoop.scheduleAtFixedRate(this, 0,
+				ITERATION_DELAY_MICROS, TimeUnit.MICROSECONDS);
 		setScheduledFuture(future);
 	}
 
@@ -257,44 +258,48 @@ public class GrpcChannelExecutor implements Runnable {
 		else {
 			// Setup round-robin load balancing.
 			NameResolver.Factory nameResolverFactory = new MultiAddressNameResolverFactory(
-					Arrays.stream(hosts)
-							.map((host) -> new InetSocketAddress(host.name, host.port))
-							.collect(
-									Collectors.<SocketAddress>toList()));
+				Arrays.stream(hosts)
+					.map((host) -> new InetSocketAddress(host.name, host.port))
+					.collect(
+						Collectors.<SocketAddress>toList()));
 			builder = NettyChannelBuilder.forTarget(String.format("%s:%d",
-					hosts[0].name, hosts[0].port));
+				hosts[0].name, hosts[0].port));
 			builder.nameResolverFactory(nameResolverFactory);
 			builder.defaultLoadBalancingPolicy("round_robin");
 		}
 
 		SingleEventLoopGroup eventLoopGroup = new SingleEventLoopGroup(eventLoop);
 		builder
-				.eventLoopGroup(eventLoopGroup)
-				.perRpcBufferLimit(128 * 1024 * 1024)
-				.channelType(channelType)
-				.negotiationType(NegotiationType.PLAINTEXT)
+			.eventLoopGroup(eventLoopGroup)
+			.perRpcBufferLimit(128 * 1024 * 1024)
+			.channelType(channelType)
+			.negotiationType(NegotiationType.PLAINTEXT)
 
-				// Execute callbacks in the assigned event loop.
-				// GrpcChannelExecutor.iterate and all of GrpcStream works on
-				// this assumption.
-				.directExecutor()
+			// Have a very large limit because this response is coming from
+			// the proxy server.
+			.maxInboundMessageSize(128 * 1024 * 1024)
 
-				// Retry logic is part of the client code.
-				.disableRetry()
+			// Execute callbacks in the assigned event loop.
+			// GrpcChannelExecutor.iterate and all of GrpcStream works on
+			// this assumption.
+			.directExecutor()
 
-				// Server and client flow control policy should be in sync.
-				.flowControlWindow(2 * 1024 * 1024)
+			// Retry logic is part of the client code.
+			.disableRetry()
 
-				// TODO: is this beneficial? See https://github.com/grpc/grpc-java/issues/8260
-				//  for discussion.
-				// Enabling this feature create too many pings and the server
-				// sends GO_AWAY response.
-				// .initialFlowControlWindow(1024 * 1024)
+			// Server and client flow control policy should be in sync.
+			.flowControlWindow(2 * 1024 * 1024)
 
-				// TODO: Should these be part of GrpcClientPolicy?
-				.keepAliveWithoutCalls(true)
-				.keepAliveTime(25, TimeUnit.SECONDS)
-				.keepAliveTimeout(1, TimeUnit.MINUTES);
+			// TODO: is this beneficial? See https://github.com/grpc/grpc-java/issues/8260
+			//  for discussion.
+			// Enabling this feature create too many pings and the server
+			// sends GO_AWAY response.
+			// .initialFlowControlWindow(1024 * 1024)
+
+			// TODO: Should these be part of GrpcClientPolicy?
+			.keepAliveWithoutCalls(true)
+			.keepAliveTime(25, TimeUnit.SECONDS)
+			.keepAliveTimeout(1, TimeUnit.MINUTES);
 
 		if (grpcClientPolicy.tlsPolicy != null) {
 			builder.sslContext(getSslContext(grpcClientPolicy.tlsPolicy));
@@ -308,7 +313,7 @@ public class GrpcChannelExecutor implements Runnable {
 		// target IP for TLS verification. A simpler way than adding a DNS
 		// entry in the hosts file.
 		String authorityProperty =
-				System.getProperty(OVERRIDE_AUTHORITY);
+			System.getProperty(OVERRIDE_AUTHORITY);
 		if (authorityProperty != null && !authorityProperty.trim().isEmpty()) {
 			builder.overrideAuthority(authorityProperty);
 		}
@@ -318,7 +323,7 @@ public class GrpcChannelExecutor implements Runnable {
 		builder.withOption(ChannelOption.SO_RCVBUF, 1048576);
 		builder.withOption(ChannelOption.TCP_NODELAY, true);
 		builder.withOption(ChannelOption.CONNECT_TIMEOUT_MILLIS,
-				grpcClientPolicy.connectTimeoutMillis);
+			grpcClientPolicy.connectTimeoutMillis);
 		builder.userAgent(AEROSPIKE_CLIENT_USER_AGENT);
 
 		// better to have a receive buffer predictor
@@ -327,7 +332,7 @@ public class GrpcChannelExecutor implements Runnable {
 		//if the server is sending 1000 messages per sec, optimum write buffer watermarks will
 		//prevent unnecessary throttling, Check NioSocketChannelConfig doc
 		builder.withOption(ChannelOption.WRITE_BUFFER_WATER_MARK,
-				new WriteBufferWaterMark(32 * 1024, 64 * 1024));
+			new WriteBufferWaterMark(32 * 1024, 64 * 1024));
 
 		ManagedChannel channel = builder.build();
 		// TODO: ensure it is a single threaded event loop.
@@ -390,7 +395,7 @@ public class GrpcChannelExecutor implements Runnable {
 		pendingCalls.forEach(call -> {
 			if (!call.hasCompleted() && call.hasExpired()) {
 				call.onError(new AerospikeException.Timeout(call.getPolicy(),
-						call.getIteration()));
+					call.getIteration()));
 			}
 		});
 
@@ -419,8 +424,10 @@ public class GrpcChannelExecutor implements Runnable {
 		bytesSent += payload.size();
 		requestsSent++;
 
+		// The stream will be close by the selector.
+		//noinspection resource
 		GrpcStream stream =
-				grpcClientPolicy.grpcStreamSelector.select(new ArrayList<>(streams.values()), call.getStreamingMethodDescriptor());
+			grpcClientPolicy.grpcStreamSelector.select(new ArrayList<>(streams.values()), call.getStreamingMethodDescriptor());
 		if (stream != null) {
 			// TODO: what if add fails
 			stream.enqueue(call);
@@ -429,7 +436,7 @@ public class GrpcChannelExecutor implements Runnable {
 
 		// Create new stream.
 		SpscUnboundedArrayQueue<GrpcStreamingCall> queue =
-				new SpscUnboundedArrayQueue<>(CALL_QUEUE_CHUNK_SIZE);
+			new SpscUnboundedArrayQueue<>(CALL_QUEUE_CHUNK_SIZE);
 		queue.add(call);
 
 		scheduleCallsOnNewStream(call.getStreamingMethodDescriptor(), queue);
@@ -469,7 +476,7 @@ public class GrpcChannelExecutor implements Runnable {
 			}
 			catch (Exception e) {
 				AerospikeException aerospikeException =
-						new AerospikeException(ResultCode.NOT_AUTHENTICATED, e);
+					new AerospikeException(ResultCode.NOT_AUTHENTICATED, e);
 				for (GrpcStreamingCall call = pendingCalls.poll();
 					 call != null;
 					 call = pendingCalls.poll()) {
@@ -491,12 +498,12 @@ public class GrpcChannelExecutor implements Runnable {
 		// - in the next call of GrpcChannelExecutor.processClosedStreams in
 		// an iteration the above steps repeat
 		SpscUnboundedArrayQueue<GrpcStreamingCall> activeCalls =
-				new SpscUnboundedArrayQueue<>(CALL_QUEUE_CHUNK_SIZE);
+			new SpscUnboundedArrayQueue<>(CALL_QUEUE_CHUNK_SIZE);
 		for (GrpcStreamingCall call = pendingCalls.poll(); call != null;
 			 call = pendingCalls.poll()) {
 			if (call.hasExpired()) {
 				call.onError(new AerospikeException.Timeout(call.getPolicy(),
-						call.getIteration()));
+					call.getIteration()));
 			}
 			else {
 				activeCalls.add(call);
@@ -508,7 +515,7 @@ public class GrpcChannelExecutor implements Runnable {
 		}
 
 		GrpcStream stream = new GrpcStream(this, methodDescriptor,
-				activeCalls, options, grpcClientPolicy, nextStreamId(), eventLoop);
+			activeCalls, options, grpcClientPolicy, nextStreamId(), eventLoop);
 
 		streams.put(stream.getId(), stream);
 		streamsOpen++;
@@ -560,7 +567,7 @@ public class GrpcChannelExecutor implements Runnable {
 		do {
 			try {
 				terminated = channel.awaitTermination(terminationWaitMillis,
-						TimeUnit.MILLISECONDS);
+					TimeUnit.MILLISECONDS);
 				break;
 			}
 			catch (InterruptedException e) {
@@ -584,16 +591,16 @@ public class GrpcChannelExecutor implements Runnable {
 	}
 
 
-    @SuppressWarnings("NonAtomicOperationOnVolatileField")
-    void onRequestCompleted() {
-        responsesReceived++;
-        ongoingRequests.getAndDecrement();
-    }
+	@SuppressWarnings("NonAtomicOperationOnVolatileField")
+	void onRequestCompleted() {
+		responsesReceived++;
+		ongoingRequests.getAndDecrement();
+	}
 
-    @SuppressWarnings("NonAtomicOperationOnVolatileField")
-    void onPayloadReceived(int size) {
-        bytesReceived += size;
-    }
+	@SuppressWarnings("NonAtomicOperationOnVolatileField")
+	void onPayloadReceived(int size) {
+		bytesReceived += size;
+	}
 
 	public long getBytesSent() {
 		return bytesSent;
@@ -616,7 +623,6 @@ public class GrpcChannelExecutor implements Runnable {
 	}
 
 	public void onStreamClosed(GrpcStream grpcStream) {
-		// TODO: will element always be added?
 		closedStreams.add(grpcStream);
 	}
 
@@ -632,15 +638,15 @@ public class GrpcChannelExecutor implements Runnable {
 		return streamsOpen;
 	}
 
-    private static class ChannelAndEventLoop {
-        final ManagedChannel managedChannel;
-        final EventLoop eventLoop;
+	private static class ChannelAndEventLoop {
+		final ManagedChannel managedChannel;
+		final EventLoop eventLoop;
 
-        private ChannelAndEventLoop(ManagedChannel managedChannel, EventLoop eventLoop) {
-            this.managedChannel = managedChannel;
-            this.eventLoop = eventLoop;
-        }
-    }
+		private ChannelAndEventLoop(ManagedChannel managedChannel, EventLoop eventLoop) {
+			this.managedChannel = managedChannel;
+			this.eventLoop = eventLoop;
+		}
+	}
 
 	public static class ChannelTypeAndEventLoop {
 		private final Class<? extends Channel> channelType;
