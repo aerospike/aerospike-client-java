38585450f0f37937dc1cef6ec4b06c83ecb8eff3
Brian Nichols <bnichols@aerospike.com>
2024-09-16 15:51:33 -0400

    CLIENT-3117 Replace an existing node in the cluster when a new peer has the same node name, but a different IP address. The existing node must also have recent cluster tend failures and not be a localhost IP address (127.0.0.1) for the node replacement to proceed.

--- client/src/com/aerospike/client/cluster/Cluster.java
+++ client/src/com/aerospike/client/cluster/Cluster.java
@@ -557,11 +557,11 @@ public class Cluster implements Runnable, Closeable {
 				}
 
 				// Handle nodes changes determined from refreshes.
-				ArrayList<Node> removeList = findNodesToRemove(peers.refreshCount);
+				findNodesToRemove(peers);
 
 				// Remove nodes in a batch.
-				if (removeList.size() > 0) {
-					removeNodes(removeList);
+				if (peers.removeList.size() > 0) {
+					removeNodes(peers.removeList);
 				}
 			}
 
@@ -750,8 +750,9 @@ public class Cluster implements Runnable, Closeable {
 		return node;
 	}
 
-	private final ArrayList<Node> findNodesToRemove(int refreshCount) {
-		ArrayList<Node> removeList = new ArrayList<Node>();
+	private final void findNodesToRemove(Peers peers) {
+		int refreshCount = peers.refreshCount;	
+		ArrayList<Node> removeList = peers.removeList;
 
 		for (Node node : nodes) {
 			if (! node.isActive()) {
@@ -785,7 +786,6 @@ public class Cluster implements Runnable, Closeable {
 				}
 			}
 		}
-		return removeList;
 	}
 
 	private final boolean findNodeInPartitionMap(Node filter) {
