527381a947eabe2181f9892b90c24abda2d020da
Mirza Karacic <mkaracic@aerospike.com>
2026-03-03 10:13:23 -0800

    Updated netty transport to release from incubator

--- examples/src/com/aerospike/examples/AsyncExample.java
+++ examples/src/com/aerospike/examples/AsyncExample.java
@@ -16,27 +16,23 @@
  */
 package com.aerospike.examples;
 
-import java.lang.reflect.Constructor;
-import java.util.List;
-
 import com.aerospike.client.AerospikeClient;
 import com.aerospike.client.Host;
 import com.aerospike.client.IAerospikeClient;
-import com.aerospike.client.async.EventLoop;
-import com.aerospike.client.async.EventLoops;
-import com.aerospike.client.async.EventPolicy;
-import com.aerospike.client.async.NettyEventLoops;
-import com.aerospike.client.async.NioEventLoops;
+import com.aerospike.client.async.*;
 import com.aerospike.client.policy.ClientPolicy;
 import com.aerospike.client.policy.Policy;
 import com.aerospike.client.policy.WritePolicy;
 import com.aerospike.client.util.Util;
-
 import io.netty.channel.EventLoopGroup;
-import io.netty.channel.epoll.EpollEventLoopGroup;
-import io.netty.channel.kqueue.KQueueEventLoopGroup;
-import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.incubator.channel.uring.IOUringEventLoopGroup;
+import io.netty.channel.MultiThreadIoEventLoopGroup;
+import io.netty.channel.epoll.EpollIoHandler;
+import io.netty.channel.kqueue.KQueueIoHandler;
+import io.netty.channel.nio.NioIoHandler;
+import io.netty.channel.uring.IoUringIoHandler;
+
+import java.lang.reflect.Constructor;
+import java.util.List;
 
 public abstract class AsyncExample {
 	/**
@@ -57,25 +53,25 @@ public abstract class AsyncExample {
 			}
 
 			case NETTY_NIO: {
-				EventLoopGroup group = new NioEventLoopGroup(1);
+                EventLoopGroup group = new MultiThreadIoEventLoopGroup(1, NioIoHandler.newFactory());
 				eventLoops = new NettyEventLoops(eventPolicy, group, params.eventLoopType);
 				break;
 			}
 
 			case NETTY_EPOLL: {
-				EventLoopGroup group = new EpollEventLoopGroup(1);
+                EventLoopGroup group = new MultiThreadIoEventLoopGroup(1, EpollIoHandler.newFactory());
 				eventLoops = new NettyEventLoops(eventPolicy, group, params.eventLoopType);
 				break;
 			}
 
 			case NETTY_KQUEUE: {
-				EventLoopGroup group = new KQueueEventLoopGroup(1);
+				EventLoopGroup group = new MultiThreadIoEventLoopGroup(1, KQueueIoHandler.newFactory());
 				eventLoops = new NettyEventLoops(eventPolicy, group, params.eventLoopType);
 				break;
 			}
 
 			case NETTY_IOURING: {
-				EventLoopGroup group = new IOUringEventLoopGroup(1);
+				EventLoopGroup group = new MultiThreadIoEventLoopGroup(1, IoUringIoHandler.newFactory());
 				eventLoops = new NettyEventLoops(eventPolicy, group, params.eventLoopType);
 				break;
 			}
