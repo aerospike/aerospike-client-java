868e3fe90c19f2c88a68e3357b61670ca2af71f0
Mirza Karacic <mkaracic@aerospike.com>
2026-07-07 11:46:49 -0700

    CLIENT-4359 Create index to use “integer” instead of “numeric” starting with Aerospike server >= 8.1.3

--- test/src/com/aerospike/test/sync/query/TestIndex.java
+++ test/src/com/aerospike/test/sync/query/TestIndex.java
@@ -22,7 +22,9 @@ import org.junit.Assume;
 import org.junit.Test;
 
 import com.aerospike.client.AerospikeException;
+import com.aerospike.client.Bin;
 import com.aerospike.client.Info;
+import com.aerospike.client.Key;
 import com.aerospike.client.ResultCode;
 import com.aerospike.client.Value;
 import com.aerospike.client.cdt.CTX;
@@ -30,7 +32,10 @@ import com.aerospike.client.cluster.Node;
 import com.aerospike.client.exp.Exp;
 import com.aerospike.client.exp.Expression;
 import com.aerospike.client.exp.LoopVarPart;
+import com.aerospike.client.query.Filter;
 import com.aerospike.client.query.IndexType;
+import com.aerospike.client.query.RecordSet;
+import com.aerospike.client.query.Statement;
 import com.aerospike.client.task.IndexTask;
 import com.aerospike.client.util.Version;
 import com.aerospike.test.sync.TestSync;
@@ -39,6 +44,9 @@ public class TestIndex extends TestSync {
 	private static final String indexName = "testindex";
 	private static final String binName = "testbin";
 	private static final String setIndexName = "testsetindex";
+	private static final String integerIndexName = "testintegerindex";
+	private static final String integerBinName = "testintegerbin";
+	private static final String integerKeyPrefix = "testintegerkey";
 
 	@Test
 	public void createDrop() {
@@ -112,6 +120,70 @@ public class TestIndex extends TestSync {
 			String cmd = IndexTask.buildStatusCommand(args.namespace, setIndexName, node.serverVersion);
 			String response = Info.request(node, cmd);
 			int code = Info.parseResultCode(response);
+			assertEquals(201, code);
+		}
+	}
+
+	@Test
+	public void integerIndexCreateQueryDrop() {
+		Assume.assumeTrue("INTEGER index type requires server version 8.1.3 or later",
+			args.serverVersion.isGreaterOrEqual(8, 1, 3, 0));
+
+		IndexTask task;
+
+		// Drop index if it already exists.
+		try {
+			task = client.dropIndex(args.indexPolicy, args.namespace, args.set, integerIndexName);
+			task.waitTillComplete();
+		}
+		catch (AerospikeException ae) {
+			if (ae.getResultCode() != ResultCode.INDEX_NOTFOUND) {
+				throw ae;
+			}
+		}
+
+		task = client.createIndex(args.indexPolicy, args.namespace, args.set, integerIndexName, integerBinName, IndexType.INTEGER);
+		task.waitTillComplete();
+
+		int size = 20;
+
+		for (int i = 1; i <= size; i++) {
+			Key key = new Key(args.namespace, args.set, integerKeyPrefix + i);
+			Bin bin = new Bin(integerBinName, i);
+			client.put(null, key, bin);
+		}
+
+		Statement stmt = new Statement();
+		stmt.setNamespace(args.namespace);
+		stmt.setSetName(args.set);
+		stmt.setBinNames(integerBinName);
+		stmt.setFilter(Filter.range(integerBinName, 4, 8));
+
+		RecordSet rs = client.query(null, stmt);
+
+		try {
+			int count = 0;
+
+			while (rs.next()) {
+				count++;
+			}
+			assertEquals(5, count);
+		}
+		finally {
+			rs.close();
+		}
+
+		task = client.dropIndex(args.indexPolicy, args.namespace, args.set, integerIndexName);
+		task.waitTillComplete();
+
+		// Ensure all nodes have dropped the index.
+		Node[] nodes = client.getNodes();
+
+		for (Node node : nodes) {
+			String cmd = IndexTask.buildStatusCommand(args.namespace, integerIndexName, node.serverVersion);
+			String response = Info.request(node, cmd);
+			int code = Info.parseResultCode(response);
+
 			assertEquals(201, code);
 		}
 	}
